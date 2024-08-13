f48e16d426777fd1c26b169c0a91da735edbe3ee
mj-song00 <niveus128@gmail.com>
2024-08-13 10:15:49 +0900

    first commit

---
+++ src/main/java/com/sparta/personal_task/PersonalTaskApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.personal_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/personal_task/PersonalTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.personal_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
